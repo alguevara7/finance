41173e89cff0165509e18d9aec3fefd75c354ad7
Alexei Guevara <alexguev@gmail.com>
2012-08-07 00:49:40 -0400

    a

--- src/finance/assignment2.clj
+++ src/finance/assignment2.clj
@@ -11,7 +11,7 @@
   [c r n]
   (* c (reduce + 1 (for [k (range (- n 1) 0 -1)] (math/expt (+ 1 r) k)))))
 
-(defn fv-pmt
+(defn pmt-fv
   "calculates the 'cost or payment' of an annuity, given a 'future value'
    'fv' future value
    'r' interest rate
@@ -29,7 +29,7 @@
   [c r n]
   (* c (reduce + (for [k (range 1 (+ n 1))] (/ 1 (math/expt (+ 1 r) k))))))
 
-(defn pv-pmt
+(defn pmt-pv
   "calculates the 'cost or payment' of an annuity, given a 'present value'
    'pv' present value
    'r' interest rate
